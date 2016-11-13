fa943ad56785fbbfb7e1efb23bd69ab5e145700e
Teppei Tamaki <leetmikeal@user.noreply.github.com>
2016-11-13 23:41:58 +0900

    added c++ project for image processing

---
+++ LmkImageClrLib/LmkImageClrLib.h
@@ -0,0 +1,13 @@
+// LmkImageClrLib.h
+
+#pragma once
+
+using namespace System;
+
+namespace LmkImageClrLib {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
