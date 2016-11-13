39f6ed4a2aa565d2eb8c5dc46a30ae51b319da30
Teppei Tamaki <leetmikeal@user.noreply.github.com>
2016-11-13 22:57:18 +0900

    Initial commit
    added general files, organized the project structure

---
+++ LmkPhotoViewer/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using GalaSoft.MvvmLight;
+
+namespace LmkPhotoViewer.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
+        }
+    }
+}
