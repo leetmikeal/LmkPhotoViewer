c73fdf1183485e9d6819c7393ef6c4a345893c8a
Teppei Tamaki <tamaki@taktpixel.co.jp>
2022-01-22 12:00:47 +0900

    fix error

--- LmkPhotoViewer/ViewModel/ViewModelLocator.cs
+++ LmkPhotoViewer/ViewModel/ViewModelLocator.cs
@@ -12,9 +12,9 @@
   See http://www.galasoft.ch/mvvm
 */
 
+using CommonServiceLocator;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
-using Microsoft.Practices.ServiceLocation;
 
 namespace LmkPhotoViewer.ViewModel
 {
@@ -42,9 +42,7 @@ namespace LmkPhotoViewer.ViewModel
             ////    SimpleIoc.Default.Register<IDataService, DataService>();
             ////}
 
-            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
             SimpleIoc.Default.Register<MainViewModel>();
-            SimpleIoc.Default.Register<AboutViewModel>();
         }
 
         public MainViewModel Main
@@ -52,14 +50,6 @@ namespace LmkPhotoViewer.ViewModel
             get
             {
                 return ServiceLocator.Current.GetInstance<MainViewModel>();
-            }
-        }
-        
-        public AboutViewModel About
-        {
-            get
-            {
-                return ServiceLocator.Current.GetInstance<AboutViewModel>();
             }
         }
         
